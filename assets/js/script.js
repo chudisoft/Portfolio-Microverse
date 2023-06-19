81e3cd65d8331c2a9b874332724c42226e6a091d
kcccendana <kc_telyn@yahoo.com>
2023-06-20 02:48:36 +0900

    modify javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,16 @@
-var nav_button = document.querySelector(".menubar");
-var nav_list = document.querySelector(".nav-items");
+let nav_button = document.querySelector(".menubar");
+let nav_list = document.querySelector(".nav-items");
+let nav_close = document.querySelector("#nav-close");
+let menuitems = Array.from(document.querySelectorAll(".menu-items"));
 
 nav_button.addEventListener('click', () => {
     nav_list.classList.toggle('show');
-
 });
-
+nav_close.addEventListener('click', () => {
+    nav_list.classList.toggle('show');
+});
+menuitems.forEach((item)=>{
+    item.addEventListener('click', () => {
+        nav_list.classList.remove('show');
+    });
+})
